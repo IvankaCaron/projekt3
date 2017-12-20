d44e20e2296a7c15d314ead6b94f5d492ca3b633
IvankaCaron <ivanka.caron@gmail.com>
2017-12-20 12:07:33 +0100

    FarmProjet, fifth commit, email reset template

--- farm/accounts/urls.py
+++ farm/accounts/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('profile/edit', views.edit_profile, name='edit_profile'),
     path('change-password', views.change_password, name='change_password'),
 
-    path('reset-password', password_reset,{'template_name': 'accounts/reset_password.html'}, name='reset_password'),
+    path('reset-password', password_reset,{'template_name': 'accounts/reset_password.html', 'email_template_name': 'accounts/reset_password_email.html'}, name='reset_password'),
 
 
     path('reset-password/done', password_reset_done, name='password_reset_done'),
